fbe0ff374a2715716e817346f2de7d1eef4fe346
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-16 01:23:45 +0200

    Update Creador_Claves.py

--- CON PYTHON/Creador_Claves.py
+++ CON PYTHON/Creador_Claves.py
@@ -1,1 +1,18 @@
+import random
 
+caracteres = 'abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXY!@$%^*&().,)0123456789'
+
+print(f"GENERADOR DE CONTRASEÑAS\n=================================\n")
+
+numero = input("¿Cuantas claves quieres generar? ")
+numero = int(numero)
+longitud = input("¿Qué longitud quiere que tengan las claves? ")
+longitud = int(longitud)
+
+print(f"\nAquí tiene sus contraseñas: ")
+
+for pwd in range(numero):
+    claves = ''
+    for c in range(longitud):
+        claves += random.choice(caracteres)
+    print(claves)
