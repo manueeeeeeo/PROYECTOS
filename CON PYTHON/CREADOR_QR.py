6a2ee033b28319ccf4eaf8dfe91e3ea25e752123
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-16 01:23:10 +0200

    Update CREADOR_QR.py

--- CON PYTHON/CREADOR_QR.py
+++ CON PYTHON/CREADOR_QR.py
@@ -1,1 +1,9 @@
+import qrcode
 
+#Ponemos lo que queremos que se vea al crear el QR
+data = 'Esto es una prueba para un QR'
+#Pedimos el nombre del archivo que generamos
+nombre = input("Digame el nombre del QR: ")
+#Procedemos a guardar la imagen
+img = qrcode.make(data)
+img.save('DIRECCIÓN_DE_DONDE_QUIRES_GUARDARLO/'+nombre)
