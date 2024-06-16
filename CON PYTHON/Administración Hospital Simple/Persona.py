42616060083f62687f1656ccf7f3b064a1432dfd
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-16 23:17:32 +0200

    Update Persona.py

--- CON PYTHON/Administración Hospital Simple/Persona.py
+++ CON PYTHON/Administración Hospital Simple/Persona.py
@@ -1,1 +1,10 @@
+class Persona:
 
+    def __init__(self, codigo, nombre, apes, edad):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.apes = apes
+        self.edad = edad
+    
+    def verInfo(self):
+        print(f"Cliente:\n{self.nombre} {self.apes}\nEdad: {self.edad}\nCódigo: {self.codigo}")
