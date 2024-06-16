e493a9503439505b40a9cf715017364f0df64610
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-16 23:31:07 +0200

    Update AdivinarNumero.py

--- CON PYTHON/AdivinarNumero.py
+++ CON PYTHON/AdivinarNumero.py
@@ -1,1 +1,18 @@
+#Agregamos las librerias necesarias
+import random
 
+#Agregamos las variables necesarias
+numero = random.randint(1, 30)
+intentos = 0
+acertaste = False
+
+while not acertaste:
+    intentos+=1
+    num = int(input("Digame un número entre el 1 y el 30: "))
+    if num > numero:
+        print(f"Un número más bajo")
+    elif num < numero:
+        print(f"Un número más alto")
+    elif num == numero:
+        print(f"MUY BIEN!!! ACERTASTE en {intentos} intentos\nEl número es: {numero}")
+        break
