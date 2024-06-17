3ae04b90de137f1be48f3aaf5be48ea22e470010
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-17 12:29:31 +0200

    Update Dado.py

--- CON PYTHON/JUEGO DADOS 1/Dado.py
+++ CON PYTHON/JUEGO DADOS 1/Dado.py
@@ -1,1 +1,11 @@
+# Importamos las librerias necesarias
+import random
 
+# Creamos una clase
+class Dado:
+
+    def __init__(self):
+        self.nCara = 0
+
+    def lanzarDado(self):
+        self.nCara = random.randint(1, 6)
