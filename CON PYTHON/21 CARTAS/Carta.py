f80427aae9bc7fa230bbb99f6973ea301727031f
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-17 12:10:05 +0200

    Update Carta.py

--- CON PYTHON/21 CARTAS/Carta.py
+++ CON PYTHON/21 CARTAS/Carta.py
@@ -1,1 +1,19 @@
+# Agregamos las librerias necesarias
+import random
 
+# Creamos una clase
+class Carta:
+
+    def __init__(self):
+        self.numero = 0
+        self.palo = ""
+    
+    def extraerCarta(self):
+        aleatorio = 8
+        palo1 = ""
+        Palos = ["Oros", "Bastos", "Espadas", "Copas"]
+        while (aleatorio==8 or aleatorio==9):
+            aleatorio = random.randint(1, 12)
+        palo1 = random.choice(Palos)
+        self.numero = aleatorio
+        self.palo = palo1
