2c315fc9226f2aba5df10bcb9400a8662fe1a0c9
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-18 21:53:57 +0200

    Update Ejercicio_basico1.py

--- CON PYTHON/MATPLOTLIB/Ejercicio_basico1.py
+++ CON PYTHON/MATPLOTLIB/Ejercicio_basico1.py
@@ -1,1 +1,15 @@
+import matplotlib.pyplot as plt
 
+squares = [1,4,9,16,25]
+fig, ax = plt.subplots() #Damos a entender que vamos a dibujar un grafico
+ax.plot(squares, linewidth=3) #Dibujamos los valores
+
+#Establecemos el título del gráfico y las etiquetas de los ejes
+ax.set_title("SQUARE NUMBERS", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+#Establecemos el tamaño de las etiquetas de los puntos de los ejes
+ax.tick_params(labelsize=14)
+
+plt.show() #MMostramos el gráfico
