d184f58c50e87a8a0516ff0fe4065bcc7723551e
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-18 22:04:56 +0200

    Update Clase.py

--- CON PYTHON/Administrador Gimnasio/Clase.py
+++ CON PYTHON/Administrador Gimnasio/Clase.py
@@ -1,1 +1,13 @@
+class Clase:
 
+    def __init__(self, codigo, nombre, precio, hora, min, duracion, apuntadas):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.precio = precio
+        self.hora = hora
+        self.min = min
+        self.duracion = duracion
+        self.apuntadas = apuntadas
+    
+    def verInfo(self):
+        print(f"CLASE {self.nombre} con código: {self.codigo} \nPrecio: {self.precio}€ \nDuración: {self.duracion}")
