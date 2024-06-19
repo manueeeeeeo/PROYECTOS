d3b8fbe2404fed66ef915f12eee7b0f5de0d8195
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-19 16:43:59 +0200

    Update app.js

--- CON JS/Reloj/js/app.js
+++ CON JS/Reloj/js/app.js
@@ -1,1 +1,18 @@
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const period = hours >= 12 ? 'PM' : 'AM';
 
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+
+    document.getElementById('hours-value').textContent = hours.toString().padStart(2, '0');
+    document.getElementById('minutes-value').textContent = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds-value').textContent = seconds.toString().padStart(2, '0');
+    document.getElementById('period').textContent = period;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
