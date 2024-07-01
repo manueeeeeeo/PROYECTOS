dce20a581d53a7e32acb028eb8bd330b818eb691
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-01 22:00:35 +0200

    Update Item.java

--- CON JAVA/GESTION GYM/src/Item.java
+++ CON JAVA/GESTION GYM/src/Item.java
@@ -1,1 +1,54 @@
+package paquete;
 
+public class Item {
+	// Variables propias de la clase
+	private String id;
+	private String nombre;
+	private double precio;
+	
+	// Constructor predefinido
+	public Item() {
+		this.id = "";
+		this.nombre = "";
+		this.precio = 0.0;
+	}
+
+	// Constructor con todos los parametros
+	public Item(String id, String nombre, double precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	// Getters y Setters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", nombre=" + nombre + ", precio=" + precio + "]";
+	}
+}
